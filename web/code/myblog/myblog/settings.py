d0281271ebe6d2b7dc30490df52df46c7e921b02
poolbooyer <ryou3018@gmail.com>
2022-05-09 22:16:33 +0900

    [fix]シークレットキー管理を修正

--- web/code/myblog/myblog/settings.py
+++ web/code/myblog/myblog/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+from .local_settings import SECRET_KEY
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(g#aj3new*l%-s_@*iz3^5!3qbffassg(*s$o#m34a7@!=y*u#'
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
